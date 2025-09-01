71ebfaafccbf01fb1b794f0d360c1f77a9aecc5e
Cam Pedersen <cam@campedersen.com>
2025-09-01 19:17:16 -0400

    feat(web/server): add Axum SSR entrypoint and static asset routes

---
+++ crates/web/src/main.rs
@@ -0,0 +1,39 @@
+#[cfg(feature = "ssr")]
+#[tokio::main]
+async fn main() {
+    use axum::Router;
+    use leptos::*;
+    use leptos_axum::{generate_route_list, LeptosRoutes};
+    use pond_web::app::*;
+    use tower::ServiceBuilder;
+    use tower_http::services::ServeDir;
+
+    simple_logger::init_with_level(log::Level::Debug).expect("couldn't initialize logging");
+
+    // Build our application with a route
+    let conf = get_configuration(None).await.unwrap();
+    let leptos_options = conf.leptos_options;
+    let addr = leptos_options.site_addr;
+    let routes = generate_route_list(App);
+
+    let app = Router::new()
+        .leptos_routes(&leptos_options, routes, App)
+        .layer(ServiceBuilder::new())
+        .nest_service("/pkg", ServeDir::new("target/site/pkg"))
+        .nest_service("/assets", ServeDir::new("target/site"))
+        .with_state(leptos_options);
+
+    let listener = tokio::net::TcpListener::bind(&addr).await.unwrap();
+    logging::log!("listening on http://{}", &addr);
+    axum::serve(listener, app.into_make_service())
+        .await
+        .unwrap();
+}
+
+#[cfg(not(feature = "ssr"))]
+pub fn main() {
+    // no client-side main function
+    // unless we want this to work with e.g., Trunk for a purely client-side app
+    // see lib.rs for hydration function instead
+}
+
